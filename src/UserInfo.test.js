b3c01341947f9771481887c411a84e0fe37e8d9d
Tim <timcpb@hotmail.com>
2021-02-05 14:40:22 +0000

    failing test - renders the users bio

--- src/UserInfo.test.js
+++ src/UserInfo.test.js
@@ -3,8 +3,15 @@ import UserInfo from "./UserInfo";
 import mockResponse from "../__mocks__/githubResponseData.json";
 
 test("renders the users name", async () => {
-  render(<UserInfo userData={mockResponse}/>);
+  render(<UserInfo userData={mockResponse} />);
 
   const element = await waitFor(() => screen.getByText("Bim Tishop"));
+  expect(element).toBeInTheDocument();
+});
+
+test("renders the users bio", async () => {
+  render(<UserInfo userData={mockResponse} />);
+
+  const element = await waitFor(() => screen.getByText("Driven, enthusiastic"));
   expect(element).toBeInTheDocument();
 });
