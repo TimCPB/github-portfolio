6fcc91ba7c4151a10b7051a781be1199ba24192d
Tim <timcpb@hotmail.com>
2021-02-05 13:41:29 +0000

    Move fetch call to App and render it in User child component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
+import fetchUserData from "./utils";
 
 import "./App.css";
 // components
 import User from "./User.js";
 
-function App() {
-  return (
-    <div className="App">
-      <Grid container spacing={3}>
-        <Grid item xs={4}>
-          <User />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {},
+    };
+  }
+
+  async componentDidMount() {
+    const json = await fetchUserData();
+    this.setState({ data: json });
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.data);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Grid container spacing={3}>
+          <Grid item xs={4}>
+            <User userData={this.state.data} />
+          </Grid>
+          <Grid item xs={8}>
+            1
+          </Grid>
+          <Grid item xs={4}>
+            2
+          </Grid>
+          <Grid item xs={4}>
+            3
+          </Grid>
+          <Grid item xs={4}>
+            4
+          </Grid>
         </Grid>
-        <Grid item xs={8}>
-          1
-        </Grid>
-        <Grid item xs={4}>
-          2
-        </Grid>
-        <Grid item xs={4}>
-          3
-        </Grid>
-        <Grid item xs={4}>
-          4
-        </Grid>
-      </Grid>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/User.js
+++ src/User.js
@@ -1,36 +1,17 @@
-import React, { Component } from "react";
-import fetchUserData from "./utils";
+import React from "react";
 
-class User extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userData: {},
-    };
-  }
-
-  async componentDidMount() {
-    const json = await fetchUserData();
-    this.setState({ userData: json });
-  }
-
-  componentDidUpdate() {
-    console.log(this.state.userData);
-  }
-
-  render() {
-    return (
-      <div className="user">
-        {this.state.userData.name}
-        <img
-          src={this.state.userData.avatar_url}
-          alt="avatar"
-          height={200}
-          width={200}
-        ></img>
-      </div>
-    );
-  }
+function User(props) {
+  return (
+    <div className="user">
+      {props.userData.name}
+      <img
+        src={props.userData.avatar_url}
+        alt="avatar"
+        height={200}
+        width={200}
+      ></img>
+    </div>
+  );
 }
 
 export default User;
